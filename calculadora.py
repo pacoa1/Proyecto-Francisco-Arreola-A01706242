5adc5dc530196386badb7bd518115e727554c48e
pacoa1 <111139724+pacoa1@users.noreply.github.com>
2022-08-19 19:15:32 -0500

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -5,4 +5,19 @@ Desplegar Menu
   Multiplicacion
   Division
 Pedir a usuario que ingrese tipo de operacion a realizar
-Si (Suma)
+Si suma
+Pedir a usuario primer numero
+Pedir a usuario segundo numero
+Desplegar suma
+Si resta
+Pedir a usuario primer numero
+Pedir a usuario segundo numero
+Desplegar resta
+Si multiplica
+Pedir a usuario primer numero
+Pedir a usuario segundo numero
+Desplegar multiplicacion
+Si division
+Pedir a usuario primer numero
+Pedir a usuario segundo numero
+Desplegar division
