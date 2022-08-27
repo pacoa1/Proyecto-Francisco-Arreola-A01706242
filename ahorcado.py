d8b6a991769916435db6afdedb262167f1b48057
pacoa1 <111139724+pacoa1@users.noreply.github.com>
2022-08-26 22:55:28 -0500

    Update and rename calculadora.py to ahorcado.py

---
+++ ahorcado.py
@@ -0,0 +1,25 @@
+print("Bienvenido al mejor juego del mundo, las reglas consisten en que se tienen tres oportunidades para adivinar la respuesta correcta. Tras cada intento se desplegara una pista")
+vidas=3
+palabra = 'manzana'
+respuesta = ''
+
+while vidas > 0 :
+    fallas = 0
+    for letra in palabra:
+        if letra in respuesta:
+            print ( letra = "")
+        else:
+         print("*")
+        fallas+=1
+    if fallas==0:
+        print("Respuesta Correcta")
+        break
+    let=input("Ingresa una letra: ")
+    respuesta+=let
+    if let not in palabra:
+        vidas-=1
+        print("Error")
+    if vidas==0:
+        print("Mal ahi")
+else:
+    print("Nos vemos pronto")
